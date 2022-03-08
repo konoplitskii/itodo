c302e103fcfba347d07e99dc86dc774bf811eb4a
sv.konoplitskii <sv.konoplitskii@gmail.com>
2022-03-08 17:49:38 +0300

    add HashRouter

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, HashRouter } from 'react-router-dom'
 import './index.scss';
 import App from './App';
 import {Provider} from "react-redux";
@@ -9,9 +9,9 @@ import {store} from "./store/store";
 
 ReactDOM.render(
     <Provider store={store}>
-         <BrowserRouter>
+         <HashRouter>
           <App />
-        </BrowserRouter>
+        </HashRouter>
     </Provider>,
   document.getElementById('todo')
 );
